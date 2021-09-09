173cc14a8b259dee2114943a0953e4fe2e2ae538
twanabrno <twana.ahmed.5595@gmail.com>
2021-09-09 13:11:24 +0300

    editing some

--- src/langs/en.js
+++ src/langs/en.js
@@ -64,5 +64,6 @@ export default {
       "A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.",
     usefull_links: "links",
     have_questions: "Have any Questions?",
+    copy_right: "All Right reserved to Tracker",
   },
 };
